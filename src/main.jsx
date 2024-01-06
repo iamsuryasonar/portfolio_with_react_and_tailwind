5dfc7501c728e1c002f17efdfcd0173142a04dd6
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-01-07 02:01:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 // import { fas } from '@fortawesome/free-solid-svg-icons';
-import ScrollToHashElement from "./components/ScrollToHashElement";
 library.add(fab);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScrollToHashElement/>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
