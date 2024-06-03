e4dee3db34db8cefb42a0e6d010e524f42e492a6
Surya Sonar <iamsuryasonar@gmail.com>
2024-06-03 20:29:30 +0530

    javascriptDSA certification added

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,14 +1,14 @@
-// import bottle from '../assets/gallery/bottle.jpg'
 import freecodecamp_backend from '../assets/certificates/freecodecampbackend.png'
 import hackerrank_frontend from '../assets/certificates/hackerrankfrontend.png'
+import freecodecamp_javascriptDSA from '../assets/certificates/freecodecamp_javascriptDSA.png'
 
 const certificates = [
     {
-        id: "freecodecampbackend",
-        title: "Freecodecamp Backend Development and Api",
-        description: "Successfully completed the backend projects including the Timestamp Microservice, Request Header Parser Microservice, URL Shortener Microservice, Exercise Tracker, and File Metadata Microservice. ",
-        image: freecodecamp_backend,
-        url: 'https://www.freecodecamp.org/certification/fccb65312fd-c2da-4f1b-8444-9e6288ad2bf3/back-end-development-and-apis'
+        id: "freecodecampjavascriptDSA",
+        title: "Freecodecamp JavaScript Algorithms and Data Structures",
+        description: "I successfully completed the JavaScript Algorithms and Data Structures certification representing ~300 hours of work, where I developed proficiency in solving complex problems using JavaScript. Here are the projects I worked on: Palindrome Checker, Roman Numeral Converter, Caesars Cipher, Telephone Number Validator, Cash Register.",
+        image: freecodecamp_javascriptDSA,
+        url: 'https://www.freecodecamp.org/certification/fccb65312fd-c2da-4f1b-8444-9e6288ad2bf3/javascript-algorithms-and-data-structures'
     },
     {
         id: "hackerrankfrontend",
@@ -17,6 +17,15 @@ const certificates = [
         image: hackerrank_frontend,
         url: 'https://www.hackerrank.com/certificates/2ef6c7a4fec4'
     },
+    {
+        id: "freecodecampbackend",
+        title: "Freecodecamp Backend Development and Api (Node, ExpressJs and MongoDb)",
+        description: "I successfully completed the backend projects including the Timestamp Microservice, Request Header Parser Microservice, URL Shortener Microservice, Exercise Tracker, and File Metadata Microservice. ",
+        image: freecodecamp_backend,
+        url: 'https://www.freecodecamp.org/certification/fccb65312fd-c2da-4f1b-8444-9e6288ad2bf3/back-end-development-and-apis'
+    },
 ]
 
 export default certificates;
+
+
