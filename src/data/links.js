b8703bd917705b12660e34d85468871c6edb35cb
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-07-23 01:05:43 +0530

    Update links.js

--- src/data/links.js
+++ src/data/links.js
@@ -2,7 +2,7 @@ const links = [
     {
         id: "resume",
         title: "My Resume - updated on 15-Apr-2024",
-        url: "https://drive.google.com/file/d/1NPCRAZUMYB2VhF1FmRWqwfbuw8bSeU2w/view?usp=drive_link",
+        url: "https://drive.google.com/file/d/1mFB83jaQFxjH05FrsYz7XVO9w73ShDpe/view?usp=drive_link",
     },
     {
         id: "LIMOSTORE",
